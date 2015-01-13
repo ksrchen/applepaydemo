2db4ee10411b614291e08f477c4df4ce451622a2
kevin chen <ksrchen@gmail.com>
2015-01-13 10:52:17 -0800

    Initial Commit

---
+++ ApplePayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ApplePayDemo
+//
+//  Created by Kevin Chen on 1/13/15.
+//  Copyright (c) 2015 Kevin Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ApplePayDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ApplePayDemo
+//
+//  Created by Kevin Chen on 1/13/15.
+//  Copyright (c) 2015 Kevin Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
