7cafb7bcf77d6f8cf8f4d2f82ffa23e3b7bcf2fc
kevin chen <ksrchen@gmail.com>
2015-01-14 12:34:55 -0800

    rfff

--- ApplePayDemo/ViewController.m
+++ ApplePayDemo/ViewController.m
@@ -7,21 +7,88 @@
 //
 
 #import "ViewController.h"
+#import <PassKit/PassKit.h>
 
 @interface ViewController ()
-
+<PKPaymentAuthorizationViewControllerDelegate>
 @end
 
 @implementation ViewController
+- (IBAction)pay:(id)sender {
+    
+    PKPaymentRequest *request = [[PKPaymentRequest alloc] init];
+    request.countryCode = @"US";
+    request.currencyCode = @"USD";
+    request.supportedNetworks = @[PKPaymentNetworkAmex, PKPaymentNetworkMasterCard, PKPaymentNetworkVisa];
+    request.merchantCapabilities = PKMerchantCapabilityEMV | PKMerchantCapability3DS;
+    request.merchantIdentifier = @"merchant.com.misty1";
+    
+    PKPaymentSummaryItem *widget1 = [PKPaymentSummaryItem summaryItemWithLabel:@"Widget 1" amount:[NSDecimalNumber decimalNumberWithString:@"0.99"]];
+    PKPaymentSummaryItem *widget2 = [PKPaymentSummaryItem summaryItemWithLabel:@"Widget 2" amount:[NSDecimalNumber decimalNumberWithString:@"1.00"]];
+    PKPaymentSummaryItem *total = [PKPaymentSummaryItem summaryItemWithLabel:@"Grand Total" amount:[NSDecimalNumber decimalNumberWithString:@"1.99"]];
+    
+    request.paymentSummaryItems = @[widget1, widget2, total];
+    
+    PKPaymentAuthorizationViewController *paymentPane = [[PKPaymentAuthorizationViewController alloc] initWithPaymentRequest:request];
+    paymentPane.delegate = self;
+    [self presentViewController:paymentPane animated:TRUE completion:nil];
+
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self setTitle:@"My Listings"];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)paymentAuthorizationViewController:(PKPaymentAuthorizationViewController *)controller
+                       didAuthorizePayment:(PKPayment *)payment
+                                completion:(void (^)(PKPaymentAuthorizationStatus status))completion {
+    
+    PKPaymentToken * token = [payment token];
+    
+    NSString * tokenString = [[NSString alloc] initWithData:[token paymentData] encoding:NSUTF8StringEncoding];
+    
+    completion( PKPaymentAuthorizationStatusSuccess);
+    [controller dismissViewControllerAnimated:YES completion:NULL];
+    
+}
+- (void)paymentAuthorizationViewControllerDidFinish: (PKPaymentAuthorizationViewController *)controller {
+    
+}
+
+#pragma mark - Table View
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 5;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    //NSDate *object = self.objects[indexPath.row];
+    UILabel * label = (UILabel *)[cell viewWithTag:2];
+    
+    label.text = @"Address";
+    return cell;
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return NO;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    }
 
 @end
